25af038295a5ddea49215c424bd04e7ae5530a3b
M0nster92 <fahimalsafi@gmail.com>
2023-07-13 22:25:49 -0400

    password service for compare and hash password

---
+++ app/src/services/password.ts
@@ -0,0 +1,21 @@
+import { scrypt, randomBytes } from 'crypto';
+import { promisify } from 'util';
+
+const scryptAsync = promisify(scrypt);
+
+export class Password {
+  static async toHash(password: String) {
+    const salt = randomBytes(8).toString('hex');
+    const passwordBuf = Buffer.from(password, 'utf-8');
+    const buf = (await scryptAsync(passwordBuf, salt, 64)) as Buffer;
+
+    return `${buf.toString('hex')}.${salt}`;
+  }
+
+  static async compare(storedPassword: string, suppliedPassword: string) {
+    const [hashedPassword, salt] = storedPassword.split('.');
+    const buf = (await scryptAsync(suppliedPassword, salt, 64)) as Buffer;
+
+    return buf.toString('hex') === hashedPassword;
+  }
+}
